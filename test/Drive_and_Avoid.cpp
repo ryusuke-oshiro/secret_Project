c22c178a851314869f9dd213df0113756090a465
Inafuku <ren-inafuku-k21h@stu.kbc.ac.jp>
2022-07-12 09:44:31 +0900

    多分できた

--- test/Drive_and_Avoid.cpp
+++ test/Drive_and_Avoid.cpp
@@ -10,6 +10,7 @@
 #include"PLAYER.h"
 #include"APPLE.h"
 #include"Common.h"
+#include"Title.h"
 
 
 /******************************************************
@@ -223,37 +224,37 @@ int WINAPI WinMain(_In_ HINSTANCE hInssance, _In_opt_ HINSTANCE
 /*********************************************
 *ゲームタイトル表示（メニュー画面）
 **********************************************/
-void DrawGameTitle(void)
-{
-	static int MenuNo = 0;
-
-	PlaySoundMem(g_TitleBGM, DX_PLAYTYPE_BACK, FALSE);
-
-	//メニューカーソル移動処理
-	if (g_KeyFlg & PAD_INPUT_DOWN) {
-		PlaySoundMem(g_SE1, DX_PLAYTYPE_BACK, TRUE);
-		if (++MenuNo > 3)MenuNo = 0;
-	}
-	if (g_KeyFlg & PAD_INPUT_UP) {
-		PlaySoundMem(g_SE1, DX_PLAYTYPE_BACK, TRUE);
-		if (--MenuNo < 0)MenuNo = 3;
-	}
-	//Zキーでメニュー選択
-	if (g_KeyFlg & PAD_INPUT_A) {
-		StopSoundMem(g_TitleBGM);
-		PlaySoundMem(g_SE2, DX_PLAYTYPE_BACK, TRUE);
-		g_GameState = MenuNo + 1;
-	}
-	//タイトル画像表示
-	DrawGraph(0, 0, g_TitleImage, FALSE);
-
-	//メニュー
-	DrawGraph(120, 200, g_Menu, TRUE);
-
-	//メニューカーソル
-	DrawRotaGraph(90, 220 + MenuNo * 40, 0.7f, M_PI / 2, g_Cone, TRUE);
-
-}
+//void DrawGameTitle(void)
+//{
+//	static int MenuNo = 0;
+//
+//	PlaySoundMem(g_TitleBGM, DX_PLAYTYPE_BACK, FALSE);
+//
+//	//メニューカーソル移動処理
+//	if (g_KeyFlg & PAD_INPUT_DOWN) {
+//		PlaySoundMem(g_SE1, DX_PLAYTYPE_BACK, TRUE);
+//		if (++MenuNo > 3)MenuNo = 0;
+//	}
+//	if (g_KeyFlg & PAD_INPUT_UP) {
+//		PlaySoundMem(g_SE1, DX_PLAYTYPE_BACK, TRUE);
+//		if (--MenuNo < 0)MenuNo = 3;
+//	}
+//	//Zキーでメニュー選択
+//	if (g_KeyFlg & PAD_INPUT_A) {
+//		StopSoundMem(g_TitleBGM);
+//		PlaySoundMem(g_SE2, DX_PLAYTYPE_BACK, TRUE);
+//		g_GameState = MenuNo + 1;
+//	}
+//	//タイトル画像表示
+//	DrawGraph(0, 0, g_TitleImage, FALSE);
+//
+//	//メニュー
+//	DrawGraph(120, 200, g_Menu, TRUE);
+//
+//	//メニューカーソル
+//	DrawRotaGraph(90, 220 + MenuNo * 40, 0.7f, M_PI / 2, g_Cone, TRUE);
+//
+//}
 /********************************************
 *ゲーム初期化処理
 *********************************************/
--- test/Title.h
+++ test/Title.h
@@ -1,1 +1,14 @@
 #pragma once
+
+class Title {
+public:
+	int g_TitleImage;
+	int g_TitleBGM;
+	int g_SE1;
+	int g_SE2;
+	int g_Menu, g_Cone;
+public:
+	void DrawGameTitle();
+};
+
+extern Title title;

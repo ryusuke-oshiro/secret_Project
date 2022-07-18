afe5e235e1422e5ced5ca77bbfc0a1cb514dcf38
ryusuke-oshiro <106945398+ryusuke-oshiro@users.noreply.github.com>
2022-07-18 23:03:49 +0900

    あ

--- test/END.cpp
+++ test/END.cpp
@@ -12,9 +12,6 @@ void END::DrawEnd() {
 	//エンド画像表示
 	DrawGraph(0, 0, g_EndImage, FALSE);
 
-	SetFontSize(24);
-	DrawString(360, 480 - 24, "Thank you for Playing", 0xffffff, 0);
-
 	//タイムの加算処理＆終了（３秒後）
 	if (++g_WaitTime > 180)g_GameState = 99;
 }

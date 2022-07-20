3c5afdcc7689f38ebb8e5da98506b2f0d6a3f323
Inafuku <ren-inafuku-k21h@stu.kbc.ac.jp>
2022-07-20 13:41:36 +0900

    aaa

--- test/Help.cpp
+++ test/Help.cpp
@@ -22,13 +22,13 @@ void HELP::DrawHelp() {
 	}
 	DrawGraph(0, 0, g_HelpImage, FALSE);
 
-	DrawOval(95, 374, 14, 7, GetColor(0, 0, 0), TRUE);
-	DrawBox(89, 375, 102, 388, GetColor(0, 0, 0), TRUE);
-	DrawOval(95, 391, 17, 7, GetColor(0, 0, 0), TRUE);
-	DrawString(92, 367, "L", GetColor(255, 255, 255));
+	DrawOval(115, 339, 14, 7, GetColor(0, 0, 0), TRUE);
+	DrawBox(109, 345, 122, 360, GetColor(0, 0, 0), TRUE);
+	DrawOval(115, 356, 17, 7, GetColor(0, 0, 0), TRUE);
+	DrawString(112, 335, "L", GetColor(255, 255, 255));
 
-	DrawOval(330, 381, 34, 14, GetColor(0, 0, 0), TRUE);
-	DrawString(310, 374, "START", GetColor(255, 255, 255));
+	DrawOval(360, 345, 34, 14, GetColor(0, 0, 0), TRUE);
+	DrawString(339, 338, "START", GetColor(255, 255, 255));
 
 	DrawCircle(100, 465, 10, GetColor(0, 0, 0), TRUE);
 	DrawString(97, 458, "B", GetColor(255, 255, 255));
